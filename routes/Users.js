18df9459bba9e7c040b99b5870f9212a1c27f993
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 10:23:40 +0530

    Update Users.js

--- routes/Users.js
+++ routes/Users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); //express
 const { fetchUserById, updateUser } = require('../controller/User');
 
 const router = express.Router();
