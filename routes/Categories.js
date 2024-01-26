0aa9e713dfb15f687dfdb1aa686b32335bdbb38c
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 10:22:53 +0530

    Update Categories.js

--- routes/Categories.js
+++ routes/Categories.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); //express
 const { fetchCategories, createCategory } = require('../controller/Category');
 
 const router = express.Router();
