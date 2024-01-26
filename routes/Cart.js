15684a775c491f2fb1d43fcb088e0b2f178f667a
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 10:22:36 +0530

    Update Cart.js

--- routes/Cart.js
+++ routes/Cart.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); //express
 const { addToCart, fetchCartByUser, deleteFromCart, updateCart } = require('../controller/Cart');
 
 const router = express.Router();
